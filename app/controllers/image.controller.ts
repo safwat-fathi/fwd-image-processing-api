82e41db83e81d9dfef8f0d70cd4ea99b39dc86fa
Safwat Fathi <safwat.rashwan@gmail.com>
2022-11-26 11:20:18 +0200

    add: caching to processed images

--- app/controllers/image.controller.ts
+++ app/controllers/image.controller.ts
@@ -6,8 +6,29 @@ export const getImage = async (req: Request, res: Response) => {
   try {
     const { filename, width, height } = req.query;
 
+    // check if image already exist
+    const imgWithDimensions = `${filename}-${width}-${height}`;
+
     const imagesDir = process.cwd() + "/images";
     const dirFiles = await fsPromises.readdir(imagesDir);
+
+    const isExist = dirFiles.find(
+      (file: string) => file.indexOf(imgWithDimensions) > -1
+    );
+
+    // console.log(dirFiles);
+    // console.log(
+    //   dirFiles.find((file: string) => file.indexOf(imgWithDimensions) > -1)
+    // );
+
+    if (isExist) {
+      console.log("does exist");
+
+      const readfile = await fsPromises.readFile(imagesDir + "/" + isExist);
+      return res.type("image/jpeg").send(readfile);
+    }
+
+    console.log("does not exist");
 
     const imgToServe = dirFiles.find(
       (file) => file.indexOf(filename as string) > -1
